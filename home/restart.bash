b1019a3754e8f625bdab24b8d199153eb0c06ab5
Benjamin J. Young <blaher09@sbcglobal.net>
2017-07-04 12:57:10 -0400

    Fixing restart script

--- home/restart.bash
+++ home/restart.bash
@@ -1,10 +1,18 @@
 # TODO: Code to check for amount of GPUs
 
-if [ ! -f /home/blaher/claymore/nohup.prev.out ]; then
+file1=`md5 /home/blaher/claymore/nohup.out`
+file2=`md5 /home/blaher/claymore/nohup.prev.out`
+
+if [ ! -f /home/blaher/claymore/nohup.prev.out ]
+then
+  echo "Creating previous output file to compare..."
   cp /home/blaher/claymore/nohup.out /home/blaher/claymore/nohup.prev.out
 else
-  if cmp /home/blaher/claymore/nohup.out /home/blaher/claymore/nohup.prev.out >/dev/null 2>&1
+  echo "Previous output file to compare exists..."
+  echo "Files: $file1 to $file2"
+  if [ "$file1" = "$file2" ]
   then
+    echo "Miner is down, restarting..."
     sudo shutdown -r now
   fi
 fi
