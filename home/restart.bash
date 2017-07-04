39069a4894a08417088a62b1d4da961042a1d265
Benjamin J. Young <blaher09@sbcglobal.net>
2017-07-04 13:21:41 -0400

    2nd try on restart script

--- home/restart.bash
+++ home/restart.bash
@@ -1,8 +1,10 @@
+echo "Starting restart check script..."
 # TODO: Code to check for amount of GPUs
 
-file1=`md5 /home/blaher/claymore/nohup.out`
-file2=`md5 /home/blaher/claymore/nohup.prev.out`
+file1=`md5sum /home/blaher/claymore/nohup.out | awk '{ print $1 }'`
+file2=`md5sum /home/blaher/claymore/nohup.prev.out | awk '{ print $1 }'`
 
+echo "Checking if previous output file exists..."
 if [ ! -f /home/blaher/claymore/nohup.prev.out ]
 then
   echo "Creating previous output file to compare..."
@@ -13,6 +15,10 @@ else
   if [ "$file1" = "$file2" ]
   then
     echo "Miner is down, restarting..."
+    cp /home/blaher/claymore/nohup.out /home/blaher/claymore/nohup.prev.out
     sudo shutdown -r now
+  else
+    echo "Miner is still running..."
+    cp /home/blaher/claymore/nohup.out /home/blaher/claymore/nohup.prev.out
   fi
 fi
